02d9fd995932672e3f43d1d20cf7fc8cd1b854a5
t_k_nguyen-admin <thuan.nguyen26104bk@hcmut.edu.vn>
2025-06-13 22:25:50 +0700

    add icon, home pic & transition

--- Webform_DK_QL/Webform_DK_QL/obj/Debug/net9.0/Webform_DK_QL.AssemblyInfo.cs
+++ Webform_DK_QL/Webform_DK_QL/obj/Debug/net9.0/Webform_DK_QL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Webform_DK_QL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8132f2405ccb85868a4024af3c1e6eb93cdad42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6d8387a639113b1698e606fef7202c3f3365838")]
 [assembly: System.Reflection.AssemblyProductAttribute("Webform_DK_QL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Webform_DK_QL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
