77a76ec8ce01abb2f1c2eb6dbc510151cb850f53
t_k_nguyen-admin <thuan.nguyen26104bk@hcmut.edu.vn>
2025-06-13 18:18:04 +0700

    create & update the web ui

--- Webform_DK_QL/Webform_DK_QL/Program.cs
+++ Webform_DK_QL/Webform_DK_QL/Program.cs
@@ -27,7 +27,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=User}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
